295367be52c3d7e9f9e5735b3ee6a5b34fba1b25
Ankit Biswakarma <ankitbishwakarmahcs2003@gmail.com>
2026-08-01 13:44:27 +0000

    done

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict" //means convert all JS code which is written in older version to newer version
+
+//alert(3+3) it will give error in JS but if we run it on browser console it will execute
+
+//console.log(3+3) console.log(3+3) it will give error.If we separate two console by comma it will not give error second move one console to next line
+
+//datatypes:- number, string boolean, (null:-standalone value), undefined, symbol=>for unique
+
+let age=18
+let name="ankit"
+
+console.log(typeof "ankit");
+console.log(typeof age)
+console.log(typeof null) // null type is object
+console.log(typeof undefined)
+
